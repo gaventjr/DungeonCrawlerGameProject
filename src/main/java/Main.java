4cdd4be5bd7e140de54eff5be6d024372c11d6d5
George Avent <gaventjr@gmail.com>
2022-09-12 15:53:10 -0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,19 @@ public class Main {
         //System objects
         Scanner in = new Scanner(System.in);
         Random rand = new Random();
+
+        //Game variables
+
+        String [] enemies = {"Skeleton", "Zombie", "Warrior", "Assassin"};
+        int maxEnemyHealth = 75;
+        int enemyAttackDamaage = 25;
+
+        //Player variables
+        int health = 100;
+        int attackDamage =50;
+        int numHealthPotions = 3;
+        int healthPotionHealAmount= 25;
+        int healthPotionDropChancePercentage =40;
     }
 
 }
