0cf98f1a03f69a285ded2fcab175fdd3d3be7df7
George Avent <gaventjr@gmail.com>
2022-09-13 16:33:10 -0400

    Builiding commands for game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,6 +19,28 @@ public class Main {
         int numHealthPotions = 3;
         int healthPotionHealAmount= 25;
         int healthPotionDropChancePercentage =40;
+
+        boolean running = true;
+
+        System.out.println("Welcome to the Dungeon! We go fun and games!");
+
+        GAME:// name of while loop
+        while(running) {
+            System.out.println("-------------------------------");
+
+            int enemyHealth = rand.nextInt(maxEnemyHealth);
+            String enemy = enemies[rand.nextInt(enemies.length)];// accessing the array
+            System.out.println("\t# " + enemy + "has appeared! $\n");// /n is like a line break to move to the next line
+
+            while(enemyHealth > 0) {
+                System.out.println("\tYour HP: " + health);// /t is tab
+                System.out.println("\t" + enemy + "'s HP: " + enemyHealth);
+                System.out.println("\n\tWhat would you like to do?");
+                System.out.println("\t1. Attack");
+                System.out.println("\t2. Drink health potion");
+                System.out.println("\t3. Run!");
+            }
+        }
     }
 
 }
